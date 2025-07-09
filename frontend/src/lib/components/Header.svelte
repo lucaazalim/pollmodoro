7ce4cc3f6a08c46282b002de50a438e9641969ba
Luca Azalim <lucaazalim@gmail.com>
2025-07-09 03:13:05 -0300

    fix: header responsiveness for small devices

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -5,19 +5,19 @@
 </script>
 
 <nav class="bg-card border-b shadow-sm">
-	<div class="mx-auto max-w-5xl px-4">
-		<div class="flex items-center justify-between py-4">
+	<div class="mx-auto max-w-5xl px-5">
+		<div class="flex items-center justify-between gap-5 py-4 max-sm:flex-col">
 			<a href="/">
 				<div class="group flex items-center gap-2">
 					<img
 						src="/pollmodoro.png"
 						alt="Pollmodoro Logo"
-						class="group-hover:-rotate-15 size-8 transition-all ease-in-out"
+						class="group-hover:-rotate-15 size-8 shrink-0 transition-all ease-in-out"
 					/>
-					<span class="text-foreground font-serif text-xl font-bold max-sm:hidden">Pollmodoro</span>
+					<span class="text-foreground font-serif text-xl font-bold">Pollmodoro</span>
 				</div>
 			</a>
-			<div class="flex flex-row items-center space-x-4">
+			<div class="flex flex-row items-center space-x-3">
 				<ThemeSwitcherButton />
 				<YourPollsButton />
 				<CreatePollButton />
